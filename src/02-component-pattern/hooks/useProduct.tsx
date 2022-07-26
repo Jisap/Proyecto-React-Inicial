f33811a66d35a859cf6f343d83d57b84a2b620f8
Jorge Matos <jisapjisap@gmail.com>
2022-07-26 18:58:45 +0200

    Control props alternativo

--- src/02-component-pattern/hooks/useProduct.tsx
+++ src/02-component-pattern/hooks/useProduct.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs{                                       // Interface para los argumentos del hook
@@ -10,20 +10,13 @@ interface useProductArgs{                                       // Interface par
 export const useProduct = ( { onChange, product, value = 0 }:useProductArgs ) => { // Este hook incrementa o decrementa la cantidad de un producto y ejecuta la función onChange cuando se cambie la cantidad
 
     const [counter, setCounter] = useState( value );            // El valor inicial del counter es el value
-    
-    const isControlled = useRef( !!onChange );                  // Si onChange existe (true), entonces isControlled es true
 
     const increaseBy = (value: number) => {                     
-
-      //Control props
-      if( isControlled.current ){                               // Si onChange existe (true), entonces se ejecuta la función onChange(count:value, product)
-        return onChange!({ count:value, product });             // El count solo mostrará el valor +1 o -1 que se le pase a la función increaseBy en productButtons
-      }                                                         // onChange -> onProductCountChange -> shoppingCart -> value -> counter 
-
-      //Sin control props
-      //const newValue = Math.max( counter + value, 0);           // newValue es el valor máximo entre 0 y el valor actual + el valor que se le pasa
-      //setCounter( newValue );                                   // El valor actual se actualiza con el nuevo valor
-      //onChange && onChange({ count: newValue, product });       // Si existe la función onChange se ejecuta con el count y el producto
+      
+      const newValue = Math.max( counter + value, 0);           // newValue es el valor máximo entre 0 y el valor actual + el valor que se le pasa
+      setCounter( newValue );                                   // El valor actual se actualiza con el nuevo valor
+      onChange && onChange({ count: newValue, product });       // Si existe la función onChange se ejecuta con el count y el producto
+    
     }
 
     useEffect(() => {
--- src/02-component-pattern/hooks/useShoppingCart.ts
+++ src/02-component-pattern/hooks/useShoppingCart.ts
@@ -10,41 +10,20 @@ export const useShoppingCart = () => {
 
         setShoppingCart(oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }; // Producto en el carrito si ya estaba o uno nuevo según argumentos
+              // Si el count del carrito = 0, se elimina el producto del carrito
+              if (count === 0) {
 
-            if (Math.max(productInCart.count + count, 0) > 0) {  // Si la cantidad de pto existente + la nueva cantidad (+/-1) es mayor a 0, entonces se actualiza el carrito
-                productInCart.count += count;                       // Se suma la cantidad al producto + la cantidad que se le pasa
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;  //Se obtiene el producto a eliminar
 
-                return {                       // Retornamos el nuevo carrito
-                    ...oldShoppingCart,         // Concatenamos el carrito anterior con el nuevo producto
-                    [product.id]: productInCart // klave: valor del producto que modificamos su cantidad
-
+                return{
+                  ...rest                                                     //Se devuelve el resto del carrito
                 }
-            }
+              }
 
-            //Si la anterior condición no se cumple, la cantidad es 0 o menor a 0, entonces se elimina el producto del carrito
-
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;                      //Se obtiene el producto a eliminar
-
-            return {
-                ...rest                                                                      //Se devuelve el resto del carrito
-            }
-
-
-            //   // Si el count del carrito = 0, se elimina el producto del carrito
-            //   if (count === 0) {
-
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;  //Se obtiene el producto a eliminar
-
-            //     return{
-            //       ...rest                                                     //Se devuelve el resto del carrito
-            //     }
-            //   }
-
-            //   // Si el count del carrito > 0, se agrega el producto al carrito
-            //   return{ 
-            //     ...oldShoppingCart,                         //Se crea un nuevo objeto con el mismo contenido que el carrito de compras
-            //     [product.id]: { ...product, count } }       //Se agrega el producto con su nuevo count
+              // Si el count del carrito > 0, se agrega el producto al carrito
+              return{ 
+                ...oldShoppingCart,                         //Se crea un nuevo objeto con el mismo contenido que el carrito de compras
+                [product.id]: { ...product, count } }       //Se agrega el producto con su nuevo count
         })
     }
 
