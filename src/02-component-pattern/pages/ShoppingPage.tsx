ee7eea135b61fd9801cf3e7af6ec5679ff2ec598
Jorge Matos <jisapjisap@gmail.com>
2022-08-01 20:14:58 +0200

    pre-npm listo

--- src/02-component-pattern/pages/ShoppingPage.tsx
+++ src/02-component-pattern/pages/ShoppingPage.tsx
@@ -1,7 +1,7 @@
 
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
 import { products } from '../data/products';
-import '../styles/custom-styles.css'
+
 
 const product = products[0]
 
@@ -17,7 +17,7 @@ export const ShoppingPage = () => {
         <ProductCard                                     
           key={ product.id }                                      
           product= { product }  // Producto que se va a mostrar                          
-          className="bg-dark"   // Clase que se aplica 
+         
           initialValues={{      // Definimos el initialValues para el productCard -> ProductCard
             count: 4,
             maxCount: 10
@@ -26,17 +26,9 @@ export const ShoppingPage = () => {
           { 
             ({ reset, count, isMaxCountReached, maxCount, increaseBy }) => ( // Se renderiza unos componentes a traves de una función que recibe un objeto con los valores generados en el productCard
               <>
-                  <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                  <ProductTitle className="text-white" />
-                  <ProductButtons className="custom-buttons"/>
-              
-                  <button onClick={reset}>Reset</button>
-                  <button onClick={ () => increaseBy(-2) }>-2</button>
-                  {
-                  (!isMaxCountReached && <button onClick={ () => increaseBy(+2) }>+2</button>)
-                  }
-                  
-                  <span>{ count } - { maxCount }</span>
+                  <ProductImage />
+                  <ProductTitle  />
+                  <ProductButtons />
               </>
             )
           }                                             
